9f1cad739ac26a9462d892a263ff9d6fd000d128
Kiadawel <kimi.weldon@gmail.com>
2020-04-24 14:20:23 -0700

    added notes from UX video + function to hide Notes

---
+++ pagefunctions.js
@@ -0,0 +1,14 @@
+//show and hide portfolio notes
+function showNotes() {
+    let notesDisplay = document.getElementById('notes');
+    let notesHider = document.getElementById('hider');
+    
+    if (notesDisplay.className == 'hideme') {
+        notesDisplay.className = 'showme';
+        notesHider.innerHTML = 'HIDE';
+    }
+    else {
+        notesDisplay.className = 'hideme';
+        notesHider.innerHTML = 'SHOW';
+    }
+}
