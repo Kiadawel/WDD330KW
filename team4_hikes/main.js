5b7352ccdaff45684c19cb7570692f2c471af90b
Kiadawel <kimi.weldon@gmail.com>
2020-05-19 14:49:24 -0700

    adding functionality

--- team4_hikes/main.js
+++ team4_hikes/main.js
@@ -3,9 +3,21 @@ import Hikes from './hikes.js';
 const myHike = new Hikes('hikeListId');
 
 // Example of using Classes and modules to organize the code needed to render our list of hikes. Not using MVC here.
+const imgBasePath = "images/";
+
+function showHikeList(){
+  const hikeListElement = document.getElementById('hikes');
+  hikeListElement.innerHTML = "";
+  renderHikeList(hikeListElement, hikeList);
+}
 
   // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
-  function renderHikeList(parent, hikes) {}
+  function renderHikeList(parent, hikes) {
+    for(hike in hikes){
+      parent.appendChild(renderOneHikeLight(hike));
+    }
+  }
+
   function renderOneHikeLight(hike) {
     const item = document.createElement("li");
     item.innerHTML = ` <h2>${hike.name}</h2>
@@ -22,8 +34,9 @@ const myHike = new Hikes('hikeListId');
     </div>`;
     return item;
   }
+
   function renderOneHikeFull(hike) {
     const item = document.createElement("li");
   
-    return item;
+    return item; 
   }
