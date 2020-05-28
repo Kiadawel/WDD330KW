d6be46f9514f3d457ed245e835f8203aff6b0fa1
Kiadawel <kimi.weldon@gmail.com>
2020-05-28 14:27:16 -0700

    rearranging again

--- todo/js/ToDos.js
+++ todo/js/ToDos.js
@@ -1,23 +1,10 @@
 import './ls.js';
 import './utilities.js';
 
-const myToDoList = new ToDos('todo');
-window.addEventListener('load', () => {
-  myToDoList.showToDoList();
-});
-
-// test function, to be removed later
-function clearLS(){
-    console.log('clearLS invoked');
-    localStorage.clear('todo');
-    console.log(localStorage.getItem('todo'));
-    myToDoList.showToDoList();
-}
-
 /************************
 * Main Class and Methods / 
 *************************/
-class ToDos {
+export default class ToDos {
     constructor(elementID) {
         this.parentElement = document.getElementById(elementID);
         this.LSkey = this.parentElement.id;
